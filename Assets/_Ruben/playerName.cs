1cf88e946819c3306a55e9b74df313c2c58627c7
Zedjukez <rubenzylom@gmail.com>
2023-06-06 09:40:38 +0200

    scriptable object character creator

---
+++ Assets/_Ruben/playerName.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "ScriptableObjects/CharacterData", order = 1)]
+public class playerName : ScriptableObject
+{
+    public string userName;
+}
